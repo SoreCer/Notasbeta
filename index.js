41eb0f595600c9ded9e4abfd4f0441cb18cb2e63
SoreCer <45244691+SoreCer@users.noreply.github.com>
2019-01-17 12:32:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs= require('fs');
+
+const restService = express();
+
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+restService.use(bodyParser.json());
+ 
+restService.post('/note', function (req,res){
+
+    var tittle = req.body.queryResult.parameters.tittle;
+    var description = req.body.queryResult.parameters.description;
+    fs.writeFile("texto", description, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+    
+    return res.json({
+        fulfillmentText: 'nota guardada',
+        source: 'webhook-echo-sample'
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
